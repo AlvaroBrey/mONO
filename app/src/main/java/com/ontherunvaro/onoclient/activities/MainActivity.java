4dbe9a57fc91015757bd68c55040a4f55dbd8c0b
ontherunvaro <alvaro.brv@gmail.com>
2016-11-12 19:22:55 +0100

    Show progress dialog while loading pages.

--- app/src/main/java/com/ontherunvaro/onoclient/activities/MainActivity.java
+++ app/src/main/java/com/ontherunvaro/onoclient/activities/MainActivity.java
@@ -1,10 +1,14 @@
 package com.ontherunvaro.onoclient.activities;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceError;
@@ -14,13 +18,16 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.ontherunvaro.onoclient.util.OnoURL;
 import com.ontherunvaro.onoclient.R;
+import com.ontherunvaro.onoclient.util.OnoURL;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private WebView webView;
+
+    private ProgressDialog progressDialog;
+    private Context context;
 
     private final static String TAG = "MainActivity";
 
@@ -28,16 +35,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        this.context = this;
         setupWebView();
     }
 
     private void setupWebView() {
         final String startURL = OnoURL.builder().withPage(OnoURL.OnoPage.CLIENT_AREA).toString();
-
-        //cookies
-        CookieSyncManager.createInstance(this);
-        CookieSyncManager.getInstance().startSync();
 
         webView = (WebView) findViewById(R.id.mainWebView);
 
@@ -63,6 +66,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class MONOWebClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            if (progressDialog == null) {
+                progressDialog = ProgressDialog.show(context, getString(R.string.dialog_loading_title), getString(R.string.dialog_loading_message));
+                progressDialog.setCancelable(false);
+            }
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+                progressDialog = null;
+            }
+        }
 
         @Override
         public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
