dc2c4cda02be25413125607c4c7922f56c19ec9d
ontherunvaro <alvaro.brv@gmail.com>
2016-11-20 12:46:23 +0100

    Add splash screen

---
+++ app/src/main/java/com/ontherunvaro/onoclient/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.ontherunvaro.onoclient.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
