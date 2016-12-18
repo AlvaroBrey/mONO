850cdfda7d46be162d3666e4fb0492045a212024
ontherunvaro <alvaro.brv@gmail.com>
2016-12-18 11:36:30 +0100

    LoginActivity.java: add checkbox to remember password

--- app/src/main/java/com/ontherunvaro/onoclient/activities/LoginActivity.java
+++ app/src/main/java/com/ontherunvaro/onoclient/activities/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.ontherunvaro.onoclient.R;
@@ -23,7 +24,35 @@ public class LoginActivity extends AppCompatActivity {
     EditText mailText;
     @BindView(R.id.edittext_login_password)
     EditText passwordText;
+    @BindView(R.id.checkbox_remember)
+    CheckBox rememberCheckbox;
 
+    @OnClick(R.id.button_login)
+    public void login() {
+        Log.d(TAG, "login: Trying to login...");
+        final String mail = mailText.getText().toString().trim();
+        final String password = passwordText.getText().toString().trim();
+
+        // TODO: 3/12/16 secure storage!
+        SharedPreferences prefs = getSharedPreferences(PrefConstants.Files.MAIN_PREFS, MODE_PRIVATE);
+        final SharedPreferences.Editor edit = prefs.edit();
+        if (rememberCheckbox.isChecked()) {
+            edit.putString(PrefConstants.Keys.USERNAME, mail);
+            edit.putString(PrefConstants.Keys.PASSWORD, Base64.encodeToString(password.getBytes(), Base64.NO_PADDING));
+        } else {
+            if (prefs.contains(PrefConstants.Keys.PASSWORD)) {
+                edit.remove(PrefConstants.Keys.PASSWORD);
+            }
+        }
+        edit.apply();
+
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra(MainActivity.EXTRA_USERNAME, mail);
+        i.putExtra(MainActivity.EXTRA_PASSWORD, password);
+        Log.d(TAG, "login: Starting main activity");
+        startActivity(i);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,27 +66,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         if (prefs.contains(PrefConstants.Keys.PASSWORD)) {
             passwordText.setText(new String(Base64.decode(prefs.getString(PrefConstants.Keys.PASSWORD, ""), Base64.NO_PADDING)));
+            rememberCheckbox.setChecked(true);
         }
-    }
-
-    @OnClick(R.id.button_login)
-    public void login() {
-        Log.d(TAG, "login: Trying to login...");
-        final String mail = mailText.getText().toString().trim();
-        final String password = passwordText.getText().toString().trim();
-
-        // TODO: 3/12/16 secure storage!
-        SharedPreferences prefs = getSharedPreferences(PrefConstants.Files.MAIN_PREFS, MODE_PRIVATE);
-        final SharedPreferences.Editor edit = prefs.edit();
-        edit.putString(PrefConstants.Keys.USERNAME, mail);
-        edit.putString(PrefConstants.Keys.PASSWORD, Base64.encodeToString(password.getBytes(), Base64.NO_PADDING));
-        edit.apply();
-
-        Intent i = new Intent(this, MainActivity.class);
-        i.putExtra(MainActivity.EXTRA_USERNAME, mail);
-        i.putExtra(MainActivity.EXTRA_PASSWORD, password);
-        Log.d(TAG, "login: Starting main activity");
-        startActivity(i);
-        finish();
     }
 }
